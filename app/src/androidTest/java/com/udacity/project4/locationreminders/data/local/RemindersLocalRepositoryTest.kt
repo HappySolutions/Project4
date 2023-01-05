414b5e9c4a9b83b4d79ec804d3551aad676c51a9
HappySolutions <heba.odesk.tasks@gmail.com>
2023-01-05 16:14:02 +0200

    implement tests on RemindersLocalRepositoryTest.kt

--- app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersLocalRepositoryTest.kt
+++ app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersLocalRepositoryTest.kt
@@ -1,22 +1,14 @@
 package com.udacity.project4.locationreminders.data.local
 
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.room.Room
-import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
+import androidx.test.platform.app.InstrumentationRegistry
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
-import org.hamcrest.CoreMatchers.`is`
-import org.hamcrest.CoreMatchers.instanceOf
-import org.hamcrest.MatcherAssert.assertThat
-import org.junit.After
-import org.junit.Before
-import org.junit.Rule
-import org.junit.Test
+import org.junit.*
 import org.junit.runner.RunWith
 
 @ExperimentalCoroutinesApi
@@ -25,6 +17,87 @@ import org.junit.runner.RunWith
 @MediumTest
 class RemindersLocalRepositoryTest {
 
-//    TODO: Add testing implementation to the RemindersLocalRepository.kt
+//Add testing implementation to the RemindersLocalRepository.kt
+    private lateinit var remDB: RemindersDatabase
+    private lateinit var remDao: RemindersDao
+    private lateinit var remRepo: RemindersLocalRepository
 
+    @Before
+    fun setup() {
+        remDB = Room.inMemoryDatabaseBuilder(
+            InstrumentationRegistry.getInstrumentation().context,
+            RemindersDatabase::class.java
+        ).allowMainThreadQueries().build()
+        remDao = remDB.reminderDao()
+        remRepo = RemindersLocalRepository(remDao)
+    }
+
+    @After
+    fun closeDB() {
+        remDB.close()
+    }
+
+    @Test
+    fun saveReminder_retrieveReminder() = runBlocking {
+
+        //save new reminder
+        val rem = ReminderDTO(
+            title = "title",
+            description = "description",
+            location = "location",
+            latitude = 0.0,
+            longitude = 0.0
+        )
+        //test save and get reminder
+        remRepo.saveReminder(rem)
+        val result = remRepo.getReminder(rem.id)
+        Assert.assertEquals(result is Result.Success, true)
+        result as Result.Success
+        Assert.assertEquals(result.data.title, rem.title)
+        Assert.assertEquals(result.data.description, rem.description)
+        Assert.assertEquals(result.data.location, rem.location)
+        Assert.assertEquals(result.data.latitude, rem.latitude)
+        Assert.assertEquals(result.data.longitude, rem.longitude)
+    }
+
+    @Test
+    fun deleteAllReminders_getReminders() = runBlocking {
+        //save new reminder
+        val rem = ReminderDTO(
+            title = "title",
+            description = "description",
+            location = "location",
+            latitude = 0.0,
+            longitude = 0.0
+        )
+        //test save then delete and get reminders
+        remRepo.saveReminder(rem)
+        remRepo.deleteAllReminders()
+        val result = remRepo.getReminders()
+        Assert.assertEquals(result is Result.Success, true)
+        result as Result.Success
+        Assert.assertEquals(result.data.isEmpty(), true)
+    }
+
+    @Test
+    fun getReminderById() = runBlocking {
+        //save new reminder
+        val rem = ReminderDTO(
+            title = "title",
+            description = "description",
+            location = "location",
+            latitude = 0.0,
+            longitude = 0.0
+        )
+        //test save then get reminder by id
+        remRepo.saveReminder(rem)
+        val result = remRepo.getReminder(rem.id)
+        Assert.assertEquals(result is Result.Success, true)
+        result as Result.Success
+        Assert.assertEquals(result.data.title, rem.title)
+        Assert.assertEquals(result.data.description, rem.description)
+        Assert.assertEquals(result.data.location, rem.location)
+        Assert.assertEquals(result.data.latitude, rem.latitude)
+        Assert.assertEquals(result.data.longitude, rem.longitude)
+    }
 }
